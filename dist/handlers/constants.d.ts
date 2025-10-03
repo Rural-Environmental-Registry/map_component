8f3e847d9b91e237e3c9a78dfd444e2d6ecf846e
Lucas Anthony <lucas.antony@gria.io>
2025-10-03 08:05:43 -0300

    feat: adicionar novos tipos e classes para gerenciamento de coordenadas e controle de desenho

---
+++ dist/handlers/constants.d.ts
@@ -0,0 +1,3 @@
+import { DrawingControlOptions, MapConfigConfig } from '../types';
+export declare const DEFAULT_MAP_OPTIONS: MapConfigConfig;
+export declare const DEFAULT_DRAW_OPTIONS: DrawingControlOptions;
